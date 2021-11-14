f5ca2de2c12cdc238d6c1ac69c3c63a56a8ddc39
Honghua Zhu <zhh200910@gmail.com>
2021-11-14 17:11:59 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,18 +1,7 @@
 @REM
-@REM  Licensed to the Apache Software Foundation (ASF) under one or more
-@REM  contributor license agreements.  See the NOTICE file distributed with
-@REM  this work for additional information regarding copyright ownership.
-@REM  The ASF licenses this file to You under the Apache License, Version 2.0
-@REM  (the "License"); you may not use this file except in compliance with
-@REM  the License.  You may obtain a copy of the License at
-@REM
-@REM      http://www.apache.org/licenses/LICENSE-2.0
-@REM
-@REM  Unless required by applicable law or agreed to in writing, software
-@REM  distributed under the License is distributed on an "AS IS" BASIS,
-@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM  See the License for the specific language governing permissions and
-@REM  limitations under the License.
+@REM  Copyright Lealone Database Group.
+@REM  Licensed under the Server Side Public License, v 1.
+@REM  Initial Developer: zhh
 
 @echo off
 if "%OS%" == "Windows_NT" setlocal
@@ -21,10 +10,14 @@ set ARG=%1
 
 if /i "%ARG%" == "" goto usage
 if /i "%ARG%" == "-e" goto e
+if /i "%ARG%" == "-ec" goto ec
 if /i "%ARG%" == "-es" goto es
 if /i "%ARG%" == "-p" goto p
 if /i "%ARG%" == "-pc" goto pc
+if /i "%ARG%" == "-pd" goto pd
 if /i "%ARG%" == "-i" goto i
+if /i "%ARG%" == "-c" goto c
+if /i "%ARG%" == "-dt" goto dt
 if /i "%ARG%" == "-vu" goto vu
 
 goto usage
@@ -33,15 +26,23 @@ goto usage
 echo usage: build [options]
 echo    options:
 echo    -e            mvn eclipse:eclipse
+echo    -ec           mvn eclipse:clean
 echo    -es           mvn eclipse:eclipse -DdownloadSources=true
 echo    -p            mvn package assembly:assembly -Dmaven.test.skip=true
 echo    -pc           mvn clean package assembly:assembly -Dmaven.test.skip=true
+echo    -pd           mvn package assembly:assembly -Dmaven.test.skip=true -P database
 echo    -i            mvn install -Dmaven.test.skip=true
+echo    -c            mvn clean
+echo    -dt           mvn dependency:tree
 echo    -vu version   pom.xml version update
 goto end
 
 :e
 call mvn eclipse:eclipse
+goto end
+
+:ec
+call mvn eclipse:clean
 goto end
 
 :es
@@ -56,8 +57,20 @@ goto end
 call mvn clean package assembly:assembly -Dmaven.test.skip=true
 goto end
 
+:pd
+call mvn package assembly:assembly -Dmaven.test.skip=true -P database
+goto end
+
+:c
+call mvn clean
+goto end
+
 :i
 call mvn install -Dmaven.test.skip=true
+goto end
+
+:dt
+call mvn dependency:tree
 goto end
 
 :vu
@@ -65,7 +78,7 @@ set VERSION=%2
 if /i "%VERSION%" == "" goto usage
 call mvn versions:set -DnewVersion=%VERSION%
 call mvn versions:commit
-echo lealoneVersion=%VERSION%>..\lealone\lealone-common\src\main\resources\org\lealone\res\version.properties
+echo lealoneVersion=%VERSION%>lealone-common\src\main\resources\org\lealone\common\resources\version.properties
 goto end
 
 :end
