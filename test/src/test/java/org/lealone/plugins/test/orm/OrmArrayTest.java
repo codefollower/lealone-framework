dfd2bd99abd25582d5d819b6ff16561a07466787
Honghua Zhu <zhh200910@gmail.com>
2022-10-25 10:04:20 +0800

    add OrmArrayTest https://github.com/lealone/Lealone-Plugins/issues/22

---
+++ test/src/test/java/org/lealone/plugins/test/orm/OrmArrayTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright Lealone Database Group.
+ * Licensed under the Server Side Public License, v 1.
+ * Initial Developer: zhh
+ */
+package org.lealone.plugins.test.orm;
+
+import java.sql.Array;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.lealone.plugins.test.orm.generated.User;
+
+public class OrmArrayTest extends OrmTestBase {
+
+    @Before
+    @Override
+    public void setUpBefore() {
+        setEmbedded(true);
+        setInMemory(true);
+        SqlScript.createUserTable(this);
+    }
+
+    @Test
+    public void run() {
+        new User().id.set(1006).name.set("rob6").phones.set(new Object[] { 1, 2, 3 }).insert();
+        new User().id.set(1007).name.set("中文").phones.set(new Object[] { 5, 6 }).insert();
+        Array array = User.dao.findArray();
+        String str = array.toString();
+        System.out.println(str);
+        assertTrue(!str.contains("STRINGDECODE"));
+    }
+
+}
